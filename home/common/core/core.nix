b663943750e5c4485a76895821dd35f9a40fbd73
Laszlo Bacsi <lackac@lackac.hu>
2025-06-02 15:49:25 +0200

    fix: type of fzf options are list of strings

--- home/common/core/core.nix
+++ home/common/core/core.nix
@@ -121,13 +121,13 @@
     # A command-line fuzzy finder
     fzf = {
       enable = true;
-      defaultCommand = "fd -type f";
+      defaultCommand = "fd --type f";
 
-      changeDirWidgetCommand = "fd -type d";
-      changeDirWidgetOptions = "--preview 'tree -C {} | head -200'";
-      fileWidgetCommand = "fd -type f";
-      fileWidgetOptions = "--preview '(highlight -O ansi -l {} 2> /dev/null || cat {} || tree -C {}) 2> /dev/null | head -200'";
-      historyWidgetOptions = "--preview 'echo {}' --preview-window down:3:hidden:wrap --bind '?:toggle-preview'";
+      changeDirWidgetCommand = "fd --type d";
+      changeDirWidgetOptions = [ "--preview 'tree -C {} | head -200'" ];
+      fileWidgetCommand = "fd --type f";
+      fileWidgetOptions = [ "--preview '(highlight -O ansi -l {} 2> /dev/null || cat {} || tree -C {}) 2> /dev/null | head -200'" ];
+      historyWidgetOptions = [ "--preview 'echo {}' --preview-window down:3:hidden:wrap --bind '?:toggle-preview'" ];
 
       tmux = {
         enableShellIntegration = true;
