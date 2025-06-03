e1a541a1bf511327efc3ff5c48119effb328e669
Laszlo Bacsi <lackac@lackac.hu>
2025-06-03 16:42:34 +0200

    fix(kitty): escape keybindings, fix remote control

--- home/common/gui/terminal/kitty.nix
+++ home/common/gui/terminal/kitty.nix
@@ -7,6 +7,7 @@
     enable = true;
 
     settings = {
+      allow_remote_control = true;
       adjust_line_height = "100%";
       adjust_column_width = "100%";
       disable_ligatures = "cursor";
@@ -18,7 +19,6 @@
     };
 
     keybindings = {
-      "allow_remote_control" = "yes";
       "kitty_mod+c" = "copy_to_clipboard";
       "kitty_mod+v" = "paste_from_clipboard";
       "kitty_mod+n" = "new_os_window";
@@ -27,19 +27,18 @@
       "kitty_mod+0" = "change_font_size all 0";
       "kitty_mod+enter" = "toggle_fullscreen";
       "kitty_mod+f5" = "load_config_file";
-      "alt+left" = "send_text all \x1b\x62";
-      "alt+right" = "send_text all \x1b\x66";
-      "alt+backspace" = "send_text all \x17";
-      "alt+b" = "send_text all \eb";
-      "alt+c" = "send_text all \ec";
-      "alt+d" = "send_text all \ed";
-      "alt+f" = "send_text all \ef";
-      "alt+t" = "send_text all \et";
-      "alt+." = "send_text all \e.";
+      "alt+left" = "send_text all \\x1b\\x62";
+      "alt+right" = "send_text all \\x1b\\x66";
+      "alt+backspace" = "send_text all \\x17";
+      "alt+b" = "send_text all \\eb";
+      "alt+c" = "send_text all \\ec";
+      "alt+d" = "send_text all \\ed";
+      "alt+f" = "send_text all \\ef";
+      "alt+t" = "send_text all \\et";
+      "alt+." = "send_text all \\e.";
     };
 
     darwinLaunchOptions = [
-      "--single-instance"
       "--listen-on=unix:~/Library/Caches/TemporaryItems/runtime/kitty"
     ];
   };
