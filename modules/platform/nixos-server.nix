6817b7ebbd73d967acddb1c349d34dcff43e8fd6
Laszlo Bacsi <lackac@lackac.hu>
2026-02-16 09:06:33 +0100

    chore: add NixOS server base module

---
+++ modules/platform/nixos-server.nix
@@ -0,0 +1,18 @@
+{ inputs, ... }: {
+  flake.modules.nixos.server = { ... }: {
+    imports = [
+      inputs.srvos.nixosModules.server
+      inputs.disko.nixosModules.disko
+      inputs.sops-nix.nixosModules.sops
+      inputs.determinate.nixosModules.default
+    ];
+
+    networking.firewall.enable = true;
+
+    nix.gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
+  };
+}
