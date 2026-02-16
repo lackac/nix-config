5af0bd6ab67b4a41f1efa9b94178f09ea878e58a
Laszlo Bacsi <lackac@lackac.hu>
2026-02-16 13:45:44 +0100

    feat(caddy): front Mattermost with DNSimple DNS-01

---
+++ modules/services/caddy.nix
@@ -0,0 +1,28 @@
+{ config, pkgs, ... }: {
+  flake.modules.nixos.caddy = {
+    services.caddy = {
+      enable = true;
+      package = pkgs.caddy.withPlugins {
+        plugins = [ "github.com/caddy-dns/dnsimple@69317c3989f098249310c981d4bc9aebe2a4b559" ];
+        hash = "sha256-J82XQgnl+K51sEY1FEb60Z+B71qyrEy00gMg2zBVJgA=";
+      };
+
+      email = "admin@lackac.hu";
+      globalConfig = ''
+        acme_dns dnsimple {
+          token {$DNSIMPLE_TOKEN}
+        }
+      '';
+      extraConfig = ''
+        mm.lackac.hu {
+          reverse_proxy 127.0.0.1:8065
+        }
+      '';
+    };
+
+    sops.secrets."dnsimple/token" = { };
+
+    systemd.services.caddy.serviceConfig.EnvironmentFile =
+      config.sops.secrets."dnsimple/token".path;
+  };
+}
