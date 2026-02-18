8e88486e19200e5c79ef3920b5be2ad9bfaa1cee
Laszlo Bacsi <lackac@lackac.hu>
2026-02-18 12:29:34 +0100

    feat(networking): route op.lackac.hu to OctoPrint over TLS

---
+++ modules/services/octoprint-proxy.nix
@@ -0,0 +1,45 @@
+{ ... }:
+{
+  flake.modules.nixos.octoprint-proxy =
+    { config, pkgs, ... }:
+    {
+      services.caddy = {
+        enable = true;
+        package = pkgs.caddy.withPlugins {
+          plugins = [ "github.com/caddy-dns/dnsimple@v0.0.0-20251214142352-69317c3989f0" ];
+          hash = "sha256-J82XQgnl+K51sEY1FEb60Z+B71qyrEy00gMg2zBVJgA=";
+        };
+
+        email = "admin@lackac.hu";
+        globalConfig = ''
+          acme_dns dnsimple {
+            api_access_token {$DNSIMPLE_API_ACCESS_TOKEN}
+          }
+        '';
+        extraConfig = ''
+          op.lackac.hu {
+            handle_path /webcam/* {
+              reverse_proxy 127.0.0.1:8080
+            }
+
+            handle {
+              reverse_proxy 127.0.0.1:5000
+            }
+          }
+        '';
+      };
+
+      sops.secrets."dnsimple/token" = { };
+
+      sops.templates."caddy/env".content = ''
+        DNSIMPLE_API_ACCESS_TOKEN=${config.sops.placeholder."dnsimple/token"}
+      '';
+
+      systemd.services.caddy.serviceConfig.EnvironmentFile = config.sops.templates."caddy/env".path;
+
+      networking.firewall.allowedTCPPorts = [
+        80
+        443
+      ];
+    };
+}
