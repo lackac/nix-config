4df5a33c8387dde20e79e65ea999a9370e6a209e
Laszlo Bacsi <lackac@lackac.hu>
2026-02-21 09:38:08 +0100

    feat: nixos-raspberrypi based image build with WiFi
    Switch from nixos-hardware to nvmd/nixos-raspberrypi for proper RPi4
    SD image support with firmware partition layout. Configure WPA supplicant
    with sops-managed PSK for headless WiFi bootstrap.

--- modules/hosts/oxygen.nix
+++ modules/hosts/oxygen.nix
@@ -2,7 +2,7 @@
 let
   targetSystem = "aarch64-linux";
 
-  sharedSpecialArgs = {
+  sharedSpecialArgs = inputs // {
     inherit inputs;
     inherit (config) vars;
   };
@@ -12,33 +12,77 @@ let
     server
     secrets
 
-    hardware-rpi4
-    (inputs.nixpkgs + "/nixos/modules/installer/sd-card/sd-image-aarch64.nix")
-
     tailscale
     octoprint
     octoprint-proxy
   ];
 
   oxygenInline =
-    { lib, ... }:
     {
+      config,
+      lib,
+      pkgs,
+      ...
+    }:
+    {
+      imports = with inputs.nixos-raspberrypi.nixosModules; [
+        sd-image
+        nixpkgs-rpi
+        raspberry-pi-4.base
+        raspberry-pi-4.display-vc4
+      ];
+
       networking.hostName = "oxygen";
       system.stateVersion = "25.11";
       sops.defaultSopsFile = ../../secrets/oxygen.yaml;
       hardware.enableAllHardware = lib.mkForce false;
+
+      networking.wireless = {
+        enable = true;
+        secretsFile = config.sops.templates."wireless/secrets".path;
+        networks."Hovirag".pskRaw = "ext:psk_home";
+      };
+
+      networking.useDHCP = lib.mkDefault true;
+      networking.interfaces.wlan0.useDHCP = lib.mkDefault true;
+
+      networking.networkmanager.enable = lib.mkForce false;
+
+      boot.loader.raspberry-pi = {
+        bootloader = "kernel";
+        firmwarePath = "/boot";
+        configurationLimit = 2;
+      };
+
+      sops.secrets."wifi/psk" = { };
+      sops.templates."wireless/secrets" = {
+        content = ''
+          psk_home=${config.sops.placeholder."wifi/psk"}
+        '';
+        owner = "root";
+        group = "root";
+        mode = "0400";
+      };
+
+      systemd.services.wpa_supplicant = {
+        after = [
+          "run-secrets.d.mount"
+        ];
+        requires = [
+          "run-secrets.d.mount"
+        ];
+      };
 
       fileSystems = {
         "/" = {
           device = "/dev/disk/by-label/NIXOS_SD";
           fsType = "ext4";
         };
-        "/boot/firmware" = {
+        "/boot" = {
           device = "/dev/disk/by-label/FIRMWARE";
           fsType = "vfat";
           options = [
             "nofail"
-            "noauto"
           ];
         };
       };
@@ -47,7 +91,7 @@ let
   oxygenModules = oxygenAspects ++ [ oxygenInline ];
 in
 {
-  flake.nixosConfigurations.oxygen = inputs.nixpkgs.lib.nixosSystem {
+  flake.nixosConfigurations.oxygen = inputs.nixos-raspberrypi.lib.nixosSystem {
     system = targetSystem;
     modules = oxygenModules;
     specialArgs = sharedSpecialArgs;
