0f725fc119e32925a2a956646e053cf59677de08
Laszlo Bacsi <lackac@lackac.hu>
2026-02-21 09:38:08 +0100

    feat: nixos-raspberrypi based image build with WiFi
    Switch from nixos-hardware to nvmd/nixos-raspberrypi for proper RPi4
    SD image support with firmware partition layout. Configure WPA supplicant
    with sops-managed PSK for headless WiFi bootstrap.

--- flake.nix
+++ flake.nix
@@ -1,8 +1,21 @@
 {
   description = "Dendritic Nix configuration for all hosts";
 
+  nixConfig = {
+    extra-substituters = [
+      "https://nixos-raspberrypi.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    nixos-raspberrypi = {
+      url = "github:nvmd/nixos-raspberrypi/main";
+    };
 
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
--- modules/services/octoprint.nix
+++ modules/services/octoprint.nix
@@ -1,37 +1,34 @@
 { ... }:
 {
-  flake.modules.nixos.octoprint = {
-    services.octoprint = {
-      enable = true;
-      host = "127.0.0.1";
-      port = 5000;
-      plugins =
-        plugins: with plugins; [
-          dashboard
-          mqtt
-          firmwareupdater
-          fullscreen
-          displaylayerprogress
-          resource-monitor
-          marlingcodedocumentation
-        ];
-      extraConfig = {
-        serial = {
-          autoconnect = true;
-          port = "/dev/serial/by-id/usb-Prusa_Research__prusa3d.com__Original_Prusa_i3_MK3_CZPX1420X004XK79619-if00";
+  flake.modules.nixos.octoprint =
+    { pkgs, ... }:
+    {
+      services.octoprint = {
+        enable = true;
+        host = "127.0.0.1";
+        port = 5000;
+        plugins =
+          plugins: with plugins; [
+            dashboard
+            mqtt
+            firmwareupdater
+            fullscreen
+            displaylayerprogress
+            resource-monitor
+            marlingcodedocumentation
+          ];
+        extraConfig = {
+          serial = {
+            autoconnect = true;
+            port = "/dev/serial/by-id/usb-Prusa_Research__prusa3d.com__Original_Prusa_i3_MK3_CZPX1420X004XK79619-if00";
+          };
         };
       };
-    };
 
-    services.mjpg-streamer = {
-      enable = true;
-      inputPlugin = "input_uvc.so -r 640x480 -f 10 -d /dev/video0";
-      outputPlugin = "output_http.so -w @www@ -n -p 8080";
-    };
 
-    users.users.octoprint.extraGroups = [
-      "dialout"
-      "video"
-    ];
-  };
+      users.users.octoprint.extraGroups = [
+        "dialout"
+        "video"
+      ];
+    };
 }
