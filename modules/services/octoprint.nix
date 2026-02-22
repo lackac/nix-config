2e5e7e1fe2bd5b36ce6054917db2a402a8daba0c
Laszlo Bacsi <lackac@lackac.hu>
2026-02-22 13:06:27 +0100

    feat(oxygen): finalize OctoPrint with FilamentManager and plugin config
    Package FilamentManager via OctoPrint overlay with SQLAlchemy 1.4,
    backports.csv, and uritools dependencies. Patch row key handling for
    Python 3.13 compatibility. Configure webcam URLs, dashboard widgets,
    and plugin defaults declaratively.

--- modules/services/octoprint.nix
+++ modules/services/octoprint.nix
@@ -1,7 +1,7 @@
 { ... }:
 {
   flake.modules.nixos.octoprint =
-    { pkgs, ... }:
+    { lib, pkgs, ... }:
     let
       rpicam-apps = pkgs.rpi.rpicam-apps.override {
         withDrmPreview = false;
@@ -9,19 +9,92 @@
         withEglPreview = false;
         withOpenCVPostProc = false;
       };
+
+      octoprint-with-filamentmanager = pkgs.octoprint.override {
+        packageOverrides =
+          self: super:
+          let
+            buildPlugin =
+              args:
+              self.buildPythonPackage (
+                args
+                // {
+                  pname = "octoprint-plugin-${args.pname}";
+                  propagatedBuildInputs = (args.propagatedBuildInputs or [ ]) ++ [ super.octoprint ];
+                  doCheck = false;
+                }
+              );
+          in
+          {
+            sqlalchemy-legacy =
+              self.callPackage "${pkgs.path}/pkgs/development/python-modules/sqlalchemy/1_4.nix"
+                { };
+
+            backports-csv = self.buildPythonPackage rec {
+              pname = "backports.csv";
+              version = "1.0.7";
+              format = "setuptools";
+
+              src = pkgs.fetchurl {
+                url = "https://files.pythonhosted.org/packages/source/b/backports.csv/backports.csv-${version}.tar.gz";
+                sha256 = "sha256-Enff/3MTCy4Qa/PdNHrbPF9sQ0CIIonYjzEkDaksvW0=";
+              };
+
+              pythonNamespaces = [ "backports" ];
+              doCheck = false;
+
+              meta = {
+                description = "CSV module backport";
+                homepage = "https://pypi.org/project/backports.csv/";
+                license = lib.licenses.psfl;
+              };
+            };
+
+            filamentmanager = buildPlugin rec {
+              pname = "filamentmanager";
+              version = "1.9.1";
+              format = "setuptools";
+
+              src = pkgs.fetchFromGitHub {
+                owner = "OllisGit";
+                repo = "OctoPrint-FilamentManager";
+                rev = version;
+                sha256 = "035h20hmsbvih0pg23qpnvk72l9vraxa2hhrll8lza7yhqjmml5w";
+              };
+
+              postPatch = ''
+                substituteInPlace octoprint_filamentmanager/data/__init__.py \
+                  --replace "row.keys()" "list(row.keys())"
+              '';
+
+              propagatedBuildInputs = with self; [
+                backports-csv
+                uritools
+                sqlalchemy-legacy
+              ];
+
+              meta = {
+                description = "OctoPrint filament inventory manager";
+                homepage = "https://github.com/OllisGit/OctoPrint-FilamentManager";
+                license = lib.licenses.agpl3Only;
+              };
+            };
+
+          };
+      };
     in
     {
       services.octoprint = {
         enable = true;
+        package = octoprint-with-filamentmanager;
         host = "127.0.0.1";
         port = 5000;
         plugins =
           plugins: with plugins; [
             dashboard
+            filamentmanager
             mqtt
-            firmwareupdater
             fullscreen
-            displaylayerprogress
             resource-monitor
             marlingcodedocumentation
           ];
@@ -34,6 +107,66 @@
             stream = "/webcam/api/stream.mjpeg?src=printer";
             snapshot = "/webcam/api/frame.jpeg?src=printer";
             ffmpeg = "${pkgs.ffmpeg-headless}/bin/ffmpeg";
+          };
+          plugins = {
+            announcements = {
+              _config_version = 1;
+              channels = {
+                _blog.read_until = 1765879200;
+                _important.read_until = 1698310200;
+                _octopi.read_until = 1746515700;
+                _plugins.read_until = 1765324800;
+                _releases.read_until = 1764593700;
+              };
+            };
+            dashboard = {
+              _config_version = 2;
+              _webcamArray = [
+                {
+                  disableNonce = false;
+                  flipH = null;
+                  flipV = null;
+                  name = "Default";
+                  rotate = null;
+                  streamRatio = "16:9";
+                  url = "/webcam/api/stream.mjpeg?src=printer";
+                }
+              ];
+              clearOn_Feedrate = "2";
+              clearOn_Filament = "2";
+              clearOn_LayerGraph = "1";
+              clearOn_PrintThumbnail = "2";
+              clearOn_PrinterMessage = "2";
+              commandWidgetArray = [
+                {
+                  command = "echo \"47.6\" | bc";
+                  enabled = false;
+                  icon = "command-icon.png";
+                  interval = "60";
+                  name = "Simulated Chamber";
+                  type = "3/4";
+                }
+              ];
+              showJobControlButtons = true;
+              showSystemInfo = true;
+              showTempGaugeColors = true;
+              showWebCam = true;
+            };
+            displaylayerprogress = {
+              showAllPrinterMessages = false;
+            };
+            classicwebcam = {
+              _config_version = 1;
+              stream = "/webcam/api/stream.mjpeg?src=printer";
+              snapshot = "/webcam/api/frame.jpeg?src=printer";
+            };
+            filamentmanager = {
+              _config_version = 1;
+              database.clientID = "ea6d5cb2-903f-11ea-bd70-dca6323bf0ec";
+            };
+            resource_monitor = {
+              _config_version = 2;
+            };
           };
         };
       };
