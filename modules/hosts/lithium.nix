5bf6262bf8bbf93e2dbab7eb3726c93ddba3086b
Laszlo Bacsi <lackac@lackac.hu>
2026-02-28 11:07:58 +0100

    feat(darwin): introduce macOS base and lithium host bootstrap

---
+++ modules/hosts/lithium.nix
@@ -0,0 +1,29 @@
+{ config, inputs, ... }:
+let
+  targetSystem = "aarch64-darwin";
+
+  sharedSpecialArgs = {
+    inherit inputs;
+    inherit (config) vars;
+  };
+
+  lithiumAspects = with inputs.self.modules.darwin; [
+    common
+  ];
+
+  lithiumInline = {
+    networking.hostName = "lithium";
+    networking.computerName = "lithium";
+    system.stateVersion = 6;
+  };
+
+  lithiumModules = lithiumAspects ++ [ lithiumInline ];
+in
+{
+  flake.darwinConfigurations.lithium = inputs.nix-darwin.lib.darwinSystem {
+    system = targetSystem;
+    modules = lithiumModules;
+
+    specialArgs = sharedSpecialArgs;
+  };
+}
