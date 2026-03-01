030cbb4289bac743737bc34a37421c3edaa86706
Laszlo Bacsi <lackac@lackac.hu>
2026-03-01 10:39:02 +0100

    feat(home): restore aliases from previous configs

--- modules/home/tools/eza.nix
+++ modules/home/tools/eza.nix
@@ -7,7 +7,18 @@
         enable = true;
         git = true;
         icons = "auto";
-        enableZshIntegration = true;
+      };
+
+      home.shellAliases = {
+        ls = "eza";
+        l = "ls -l";
+        la = "ls -a";
+        lla = "ls -la";
+        lk = "eza -l --sort=size";
+        lt = "eza -l --sort=modified";
+        lc = "eza -l --sort=changed";
+        lu = "eza -l --sort=accessed";
+        tree = "eza --tree";
       };
     };
 }
