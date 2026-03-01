b49c9f564c8933c0261fb6a262fbe676a2015a65
Laszlo Bacsi <lackac@lackac.hu>
2026-03-01 07:42:47 +0100

    feat(home): manage git workflow via Home Manager module

---
+++ modules/home/tools/git.nix
@@ -0,0 +1,112 @@
+{ ... }:
+{
+  flake.modules.homeManager.git =
+    { vars, pkgs, ... }:
+    {
+      home.packages = with pkgs; [
+        delta
+        git-lfs
+        gh
+        tea
+      ];
+
+      home.shellAliases = {
+        gst = "git status -sb";
+        gl = "git log --decorate --graph --pretty=\"%C(auto)%h%d %C(bold)%s %C(blue)%ar%Creset %ad\" --date=iso";
+        glb = "gl --branches";
+        glp = "git log -p --decorate";
+        gco = "git checkout";
+        gc = "git commit -v";
+        gca = "git commit -v -a";
+        gcp = "git cherry-pick";
+        gp = "git push";
+        gpu = "git push -u origin";
+        gll = "git pull";
+        gsps = "git stash && git pull && git stash pop";
+      };
+
+      programs.git = {
+        enable = true;
+        userName = vars.fullName;
+        userEmail = vars.email;
+
+        aliases = {
+          br = "branch";
+          co = "checkout";
+          st = "status";
+          dc = "diff --cached";
+          update = "pull --rebase --autostash";
+          foreach = "submodule foreach";
+          start = "!git init && git commit --allow-empty -m \"chore: inception\"";
+          human = "name-rev --name-only --refs=refs/heads/*";
+          humin = "name-rev --refs=refs/heads/* --stdin";
+          ls = "log --pretty=format:'%C(yellow)%h%Cred%d %Creset%s%Cblue [%cn]' --decorate";
+          ll = "log --pretty=format:'%C(yellow)%h%Cred%d %Creset%s%Cblue [%cn]' --decorate --numstat";
+          conflicts = "diff --name-only --diff-filter=U";
+          resolve = "!git ls-files --unmerged | cut -c51- | sort -u | xargs git add";
+        };
+
+        extraConfig = {
+          user.signingkey = "~/.ssh/git-signing.pub";
+          url."ssh://git@github.com/".insteadOf = "https://github.com/";
+
+          gpg.format = "ssh";
+          commit.gpgsign = true;
+          tag.gpgsign = true;
+
+          init.defaultBranch = "main";
+          fetch.prune = true;
+
+          push = {
+            autoSetupRemote = true;
+            default = "simple";
+            followTags = true;
+          };
+
+          pull.rebase = true;
+          branch.autosetuprebase = "always";
+          merge.conflictstyle = "zdiff3";
+          rerere.enabled = true;
+
+          rebase = {
+            autosquash = true;
+            autostash = true;
+          };
+
+          core.pager = "delta";
+          interactive.diffFilter = "delta --color-only";
+
+          delta = {
+            navigate = true;
+            diff-so-fancy = true;
+            line-numbers = true;
+            true-color = "always";
+          };
+
+          diff = {
+            algorithm = "histogram";
+            colorMoved = "default";
+            colorMovedWS = "allow-indentation-change";
+            submodule = "log";
+          };
+
+          status.submoduleSummary = true;
+          submodule.recurse = true;
+        };
+      };
+
+      programs.lazygit = {
+        enable = true;
+        settings = {
+          gui = {
+            showIcons = true;
+            nerdFontsVersion = "3";
+            showCommandLog = false;
+          };
+
+          git.autoFetch = false;
+          confirmOnQuit = true;
+        };
+      };
+    };
+}
