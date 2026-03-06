6101af0d6b677411c3fd18bb046f7c93605e37ba
Laszlo Bacsi <lackac@lackac.hu>
2026-03-06 13:56:00 +0100

    feat(onepassword): consolidate desktop auth tooling into host aspect

---
+++ modules/home/one-password.nix
@@ -0,0 +1,47 @@
+{ config, inputs, ... }:
+let
+  inherit (config) vars;
+in
+{
+  flake.modules.darwin.onePassword = {
+    homebrew.casks = [
+      "1password"
+      "1password-cli"
+    ];
+
+    home-manager.users.${vars.username}.imports = [ inputs.self.modules.homeManager.onePassword ];
+  };
+
+  flake.modules.homeManager.onePassword =
+    {
+      lib,
+      config,
+      ...
+    }:
+    let
+      opAgentSock = "${config.home.homeDirectory}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+      opPluginsPath = "${config.xdg.configHome}/op/plugins.sh";
+    in
+    {
+      home.sessionVariables.SSH_AUTH_SOCK = opAgentSock;
+
+      programs.ssh.matchBlocks."*".identityAgent = opAgentSock;
+
+      programs.bash.bashrcExtra = lib.mkAfter ''
+        if [ -f "${opPluginsPath}" ]; then
+          source "${opPluginsPath}"
+        fi
+      '';
+
+      programs.zsh.initContent = lib.mkAfter ''
+        if [ -f "${opPluginsPath}" ]; then
+          source "${opPluginsPath}"
+        fi
+
+        if command -v op &>/dev/null; then
+          eval "$(op completion zsh)"
+          compdef _op op
+        fi
+      '';
+    };
+}
--- modules/hosts/lithium.nix
+++ modules/hosts/lithium.nix
@@ -16,6 +16,7 @@ let
     hammerspoon
     homebrew
     home
+    onePassword
   ];
 
   lithiumInline = {
