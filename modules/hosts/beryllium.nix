4a241b2515aaae0667b30e07a5918bafdfb0b844
Laszlo Bacsi <lackac@lackac.hu>
2026-03-09 18:36:58 +0100

    feat(darwin): add beryllium backup workstation host

---
+++ modules/hosts/beryllium.nix
@@ -0,0 +1,36 @@
+{ config, inputs, ... }:
+let
+  targetSystem = "aarch64-darwin";
+
+  sharedSpecialArgs = {
+    inherit inputs;
+    inherit (config) vars;
+  };
+
+  berylliumAspects = with config.flake.modules.darwin; [
+    common
+    desktop
+    disable-hotkeys
+    fonts
+    hammerspoon
+    home
+    keyboard-layout
+    onePassword
+  ];
+
+  berylliumInline = {
+    networking.hostName = "beryllium";
+    networking.computerName = "beryllium";
+    system.stateVersion = 6;
+  };
+
+  berylliumModules = berylliumAspects ++ [ berylliumInline ];
+in
+{
+  flake.darwinConfigurations.beryllium = inputs.nix-darwin.lib.darwinSystem {
+    system = targetSystem;
+    modules = berylliumModules;
+
+    specialArgs = sharedSpecialArgs;
+  };
+}
