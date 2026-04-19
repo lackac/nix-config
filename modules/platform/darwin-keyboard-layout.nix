b1a77ce8c60c0381a2d952d7c5f6360c4809bae1
Laszlo Bacsi <lackac@lackac.hu>
2026-04-19 23:02:43 +0200

    fix(darwin): working ExtHun keyboard bundle

--- modules/platform/darwin-keyboard-layout.nix
+++ modules/platform/darwin-keyboard-layout.nix
@@ -3,11 +3,11 @@
   flake.modules.darwin.keyboard-layout =
     { ... }:
     {
-      system.activationScripts.keyboard-layouts.text = lib.mkAfter ''
-        echo "Installing keyboard layouts..." >&2
-        install -d -m755 "/Library/Keyboard Layouts"
-        install -m644 "${./keyboard-layouts/abc-exthun.keylayout}" "/Library/Keyboard Layouts/ABC – ExtHun.keylayout"
-        install -m644 "${./keyboard-layouts/abc-exthun.icns}" "/Library/Keyboard Layouts/ABC – ExtHun.icns"
+      system.activationScripts.extraActivation.text = lib.mkAfter ''
+        rm -rf "/Library/Keyboard Layouts/ABC – ExtHun.bundle"
+        rm -f "/Library/Keyboard Layouts/ABC – ExtHun.keylayout"
+        rm -f "/Library/Keyboard Layouts/ABC – ExtHun.icns"
+        cp -R "${./keyboard-layouts/abc-exthun.bundle}" "/Library/Keyboard Layouts/ABC – ExtHun.bundle"
       '';
 
       system.defaults.CustomUserPreferences."com.apple.HIToolbox" = {
@@ -18,14 +18,14 @@
           }
           {
             InputSourceKind = "Keyboard Layout";
-            "KeyboardLayout ID" = -4797;
+            "KeyboardLayout ID" = -30646;
             "KeyboardLayout Name" = "ABC – ExtHun";
           }
         ];
         AppleSelectedInputSources = [
           {
             InputSourceKind = "Keyboard Layout";
-            "KeyboardLayout ID" = -4797;
+            "KeyboardLayout ID" = -30646;
             "KeyboardLayout Name" = "ABC – ExtHun";
           }
         ];
