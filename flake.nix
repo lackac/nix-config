4b92426745e7ce98b456c985fbe8b669cf9672ef
Laszlo Bacsi <lackac@lackac.hu>
2026-06-27 09:09:04 +0200

    chore: bump oc-config

--- flake.nix
+++ flake.nix
@@ -4,9 +4,11 @@
   nixConfig = {
     extra-substituters = [
       "https://nixos-raspberrypi.cachix.org"
+      "https://cache.numtide.com"
     ];
     extra-trusted-public-keys = [
       "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
+      "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
     ];
   };
 
